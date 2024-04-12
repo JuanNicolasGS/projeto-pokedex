c89b0d8c8ffe034dac0422b2203fb9108d83956c
Juan Nicolas <juan.nicolas@academico.ifpb.edu.br>
2024-04-12 15:41:38 -0300

    card ao centro e cores nas string

--- JS/index.js
+++ JS/index.js
@@ -8,13 +8,13 @@ const moves = document.getElementById("moves")
 const items = document.getElementById("items")
 var card = document.getElementById("card")
 
-fetch(api+"pokemon/94")
+fetch(api+"pokemon/4")
     .then(res => res.json())
     .then(json => {
         console.log(json)
         pokeimg.src = json.sprites.other["official-artwork"].front_default
         poke.innerHTML = json.name
-        types.innerHTML = "Tipos: "+ json.types[0].type.name + ", " + json.types[1].type.name
+        types.innerHTML = "Tipos: "+ json.types[0].type.name
         abilities.innerHTML = "Habilidades: " + json.abilities[0].ability.name
         stats.innerHTML = "Stats: " + json.stats[0].base_stat + ", " + json.stats[1].base_stat + 
         ", " + json.stats[2].base_stat + ", " + json.stats[3].base_stat + ", "

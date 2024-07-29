c4466395bcc700cdeec9f6f242028a926257641a
Juan Nicolas <juan.nicolas@academico.ifpb.edu.br>
2024-07-29 13:53:29 -0300

    :sparkles: feat: adicionando header

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <header className='flex'>
+            <img src="./public/pokeball.svg" className='w-9 p-0.5 bg-zinc-800 rounded-full m-2 shadow-md' alt="" />
+            <input type="search" placeholder='Name or ID' className='w-3/4 rounded-full p-4 h-2 bg-zinc-800 m-2.5 shadow-md' />
+            {/* <svg xmlns="http://www.w3.org/2000/svg" id="button-search" width="16" height="16" fill="#a0a0a065"
+                class="bi bi__search" viewBox="0 0 16 16">
+                <path
+                  d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg> */}
+            <div className='flex ml-2 bg-zinc-800 rounded-full p-0 '>
+              <a href="" className='w-9 '>
+                <img src="./public/discord.svg" className='w-6 m-2 mt-3.5 ' alt="" />
+              </a>
+              <a href="" className='w-9 '>
+                <img src="./public/houses.svg" className='w-6 m-2 mt-3.5' alt="" />
+              </a>
+              <a href="" className='w-9 '>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="rgb(256, 256, 256)" class="bi bi-list" className='m-2 mt-3'  viewBox="0   0 16 16">
+                  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5  0  0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                </svg>
+              </a>
+            </div>
+        </header>
+      </nav>
     </>
   )
 }

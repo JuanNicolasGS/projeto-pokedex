60c2c991c4b1214245cffff27fc788d68a758cfe
Isaac Yuri <isaacyuri22@gmail.com>
2024-08-02 15:43:36 -0300

    :recycle: refactor: Componente Card adicionado

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,88 +1,120 @@
+import Card from "../Card";
+
+const pokemons = [
+    {
+        numero: "#0001",
+        nome: "Bulbasaur",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+        cor: "bg-green-500"
+    },
+    {
+        numero: "#0002",
+        nome: "Ivysaur",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png",
+        cor: "bg-green-500"
+    },
+    {
+        numero: "#0003",
+        nome: "Venusaur",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png",
+        cor: "bg-green-500"
+    },
+    {
+        numero: "#0004",
+        nome: "Charmander",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png",
+        cor: "bg-red-400"
+    },
+    {
+        numero: "#0005",
+        nome: "Charmeleon",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png",
+        cor: "bg-red-400"
+    },
+    {
+        numero: "#0006",
+        nome: "Charizard",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/6.png",
+        cor: "bg-red-400"
+    },
+    {
+        numero: "#0007",
+        nome: "Squirtle",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/7.png",
+        cor: "bg-blue-400"
+    },
+    {
+        numero: "#0008",
+        nome: "Wartortle",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/8.png",
+        cor: "bg-blue-400"
+    },
+    {
+        numero: "#0009",
+        nome: "Blastoise",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/9.png",
+        cor: "bg-blue-400"
+    },
+    {
+        numero: "#0010",
+        nome: "Caterpie",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/10.png",
+        cor: "bg-green-500"
+    },
+    {
+        numero: "#0011",
+        nome: "Metapod",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/11.png",
+        cor: "bg-green-500"
+    },
+    {
+        numero: "#0012",
+        nome: "Butterfree",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/12.png",
+        cor: "bg-slate-400"
+    },
+    {
+        numero: "#0013",
+        nome: "Weedle",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/13.png",
+        cor: "bg-orange-400"
+    },
+    {
+        numero: "#0014",
+        nome: "Kakuna",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/14.png",
+        cor: "bg-yellow-400"
+    },
+    {
+        numero: "#0015",
+        nome: "Beedrill",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/15.png",
+        cor: "bg-yellow-400"
+    },
+    {
+        numero: "#0016",
+        nome: "Pidgey",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/16.png",
+        cor: "bg-orange-400"
+    }
+];
+
 export default function Main() {
-    return(
-        <div className="flex flex-col h-full w-full justify-center items-center">
+
+
+    return (
+        <main className="flex flex-col h-full w-full justify-center items-center">
             <div className="w-11/12 h-full bg-zinc-800 grid grid-cols-2 grid-rows-4 gap-4 rounded-xl p-4">
-                <div className="h-28 p-4 bg-green-500 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0001</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Bulbasaur</span>
-                </div>
-                <div className="h-28 p-4 bg-green-500 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0002</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Ivysaur</span>
-                </div>
-                <div className="h-28 p-4 bg-green-500 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0003</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Venosaur</span>
-                </div>
-                <div className="h-28 p-4 bg-red-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0004</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Charmander</span>
-                </div>
-                <div className="h-28 p-4 bg-red-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0005</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Charmeleon</span>
-                </div>
-                <div className="h-28 p-4 bg-red-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0006</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/6.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Charizard</span>
-                </div>
-                <div className="h-28 p-4 bg-blue-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0007</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/7.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Squirtle</span>
-                </div>
-                <div className="h-28 p-4 bg-blue-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0008</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/8.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Wartortle</span>
-                </div>
-                <div className="h-28 p-4 bg-blue-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0009</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/9.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Blastoise</span>
-                </div>
-                <div className="h-28 p-4 bg-green-500 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0010</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/10.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Caterpie</span>
-                </div>
-                <div className="h-28 p-4 bg-green-500 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0011</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/11.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Metapod</span>
-                </div>
-                <div className="h-28 p-4 bg-slate-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0012</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/12.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Butterfree</span>
-                </div>
-                <div className="h-28 p-4 bg-orange-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0013</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/13.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Weedle</span>
-                </div>
-                <div className="h-28 p-4 bg-yellow-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0014</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/14.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Kakuna</span>
-                </div>
-                <div className="h-28 p-4 bg-yellow-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0015</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/15.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Beedrill</span>
-                </div>
-                <div className="h-28 p-4 bg-orange-400 rounded-md flex items-center justify-center flex-col relative">
-                    <span className="text-zinc-200 absolute top-2 right-3 font-bold">#0016</span>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/16.png" className="w-24" alt="" />
-                    <span className="text-zinc-100 absolute bottom-2 left-3 font-bold">Pidgey</span>
-                </div>
+                {pokemons.map(pokemon => 
+                    <Card
+                        key={pokemon.numero}
+                        nome={pokemon.nome}
+                        imagem={pokemon.imagem}
+                        cor={pokemon.cor}
+                        numero={pokemon.numero}
+                    />
+                )}
             </div>
-        </div>
+        </main>
     )
 }

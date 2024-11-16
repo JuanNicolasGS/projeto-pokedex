85bb5e31f761d6e4014bc4e41f132196f4ea3fab
Jonatas Natan <jonatas.22ti@gmail.com>
2024-11-16 13:30:14 -0300

    :recycle: refactor: Adicionando sistema de paginação para implementação da api

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,38 @@
-const fetchPokemonData = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
-  const data = await response.json();
+const fetchPokemonData = async (page = 1) => {
+  const limit = 20;
+  const offset = (page - 1) * limit;
 
-  const pokemonDetails = await Promise.all(
-    data.results.map(async (pokemon) => {
-      const response = await fetch(pokemon.url);
-      const infoPoke = await response.json();
+  try {
+    // Faz a requisição para a API para pegar a lista de Pokémon
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+    );
+    const data = await response.json();
 
-      const speciesResponse = await fetch(infoPoke.species.url);
-      const speciesData = await speciesResponse.json();
-      const color = speciesData.color.name;
+    // Processa os detalhes de cada Pokémon
+    const pokemonDetails = await Promise.all(
+      data.results.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const infoPoke = await response.json();
 
-      // Retornando os dados estruturados
-      return {
-        name: infoPoke.name,
-        id: infoPoke.id,
-        sprites: infoPoke.sprites.other.home.front_default,
-        color: color,
-      };
-    })
-  );
+        const speciesResponse = await fetch(infoPoke.species.url);
+        const speciesData = await speciesResponse.json();
+        const color = speciesData.color.name;
 
-  console.log(pokemonDetails);
+        return {
+          name: infoPoke.name,
+          id: infoPoke.id,
+          sprites: infoPoke.sprites.other.home.front_default,
+          color: color,
+        };
+      })
+    );
+
+    return console.log(pokemonDetails); // Retorna os dados de todos os Pokémon
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+    return []; // Se der erro, retorna um array vazio
+  }
 };
 
-fetchPokemonData();
+export default fetchPokemonData();
